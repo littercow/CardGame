c4431d3b1a415344d9a0b68d08fabc00b832e318
littercow <50421468+littercow@users.noreply.github.com>
2022-05-18 02:24:17 -0400

    auto update

--- src/cardgame/Cardgame.java
+++ src/cardgame/Cardgame.java
@@ -18,7 +18,7 @@ public class Cardgame {
         CardDeck deck1 = new CardDeck();
         //System.out.println(deck1);
         deck1.shuffleCards();
-        //System.out.println(deck1);
+        System.out.println(deck1);
         Card draw1 = deck1.drawCard();
         Card draw2 = deck1.drawCard();
         Card draw3 = deck1.drawCard();
